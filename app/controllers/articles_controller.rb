2db912477afbc7da37bf8a9e78e84741e7bf9edf
kierepkae <kierepkaedyta@gmail.com>
2019-09-01 14:30:27 +0100

    add validation for article title to be min 5 characters, user see and error if hes trying to put shorte or no title at all

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
   def new
+    @article = Article.new
   end
 
   def index
@@ -7,8 +8,11 @@ class ArticlesController < ApplicationController
   end
   def create
     @article = Article.new(article_params)
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      render :new
+    end
   end
 
   def show
